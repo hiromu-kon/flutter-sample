516e11905d0d244bacb224353b482418d00b33bf
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2021-09-29 22:43:19 +0900

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -36,41 +36,36 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        bottomOpacity: 0.0,
+        elevation: 0.0,
+        backgroundColor: Colors.white,
         title: Text(
-          'テスト',
-          style: TextStyle(
-              color: onPressed == true ? Colors.redAccent : Colors.black87),
+          'Twitter Clone',
+          style: TextStyle(color: Colors.blue),
         ),
       ),
       drawer: Drawer(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [Icon(Icons.add_a_photo)],
         ),
       ),
       body: Column(
         children: [
-          Container(
-            width: 100,
-            height: 100,
-            child: Image.network(
-                'https://images.unsplash.com/photo-1586227740560-8cf2732c1531?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1861&q=80'),
-          ),
-          IconButton(
-              onPressed: () {
-                setState(() {
-                  onPressed = !onPressed;
-                  print(onPressed);
-                });
-              },
-              icon: Icon(Icons.input)),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context, MaterialPageRoute(builder: (c) => NextPage()));
-              },
-              child: Text('次のページへ')),
+          Text('まだ何も投稿されていません'),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.article_outlined),
+        onPressed: () {},
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), title: Text('ホーム')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined), title: Text('ホーム')),
         ],
       ),
     );
